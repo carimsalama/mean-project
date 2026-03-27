6ce620c561a1c95e56245bd62cbb7e9b7a8fc191
carimsalama <96595524+carimsalama@users.noreply.github.com>
2026-03-27 17:10:41 +0200

    Refactor user model

--- mean-backend/models/user.model.js
+++ mean-backend/models/user.model.js
@@ -41,14 +41,14 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-// Hash password before saving
+
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next;
   this.password = await bcrypt.hash(this.password, 2);
   next;
 });
 
-// Compare password method
+
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
